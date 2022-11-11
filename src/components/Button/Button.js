c7a4e773d74d88b77867de444622da4ea74c188a
HanmaDaiki <daikihanma@outlook.com>
2022-11-11 06:21:40 +0300

    add, fix: button, button-styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-const Button = ({ buttonText, onClick }) => {
+const Button = ({ buttonText, buttonType='button', onClick, type }) => {
   return (
-    <button onClick={ onClick } className='button'>
+    <button type={buttonType} onClick={ onClick } className={`button button_type_${type}`}>
       { buttonText }
     </button>
   );
