e3e74091aeec8cacdb06287f1d2d35a21d24adb1
HanmaDaiki <daikihanma@outlook.com>
2022-11-12 14:31:30 +0300

    add: login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
 import Fieldset from '../Fieldset/Fieldset';
 import './Login.css';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  function handleClickReg() {
+    navigate('/signup');
+  };
+
   const inputs = [
     {
       name: 'E-mail',
@@ -19,8 +26,13 @@ const Login = () => {
 
   return (
     <form className='login'>
-      <Fieldset inputs={inputs}/>
-      <Button buttonText='Войти' buttonType='submit' type={`form`} />
+      <Fieldset 
+        inputs={ inputs } 
+        text='Рады видеть!' 
+        buttonText='Войти' 
+        subText={`Ещё не зарегистрированы?`}
+        subButton={<Button buttonText='Регистрация' onClick={ handleClickReg } type='link-form' /> }
+        />
     </form>
   );
 }
