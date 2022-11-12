21cdf07b24d110b3e46210dc4a62f9b4c51341d7
HanmaDaiki <daikihanma@outlook.com>
2022-11-12 14:30:58 +0300

    fix: main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import AboutMe from '../AboutMe/AboutMe';
 import AboutProject from '../AboutProject/AboutProject';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import Techs from '../Techs/Techs';
 import './Main.css';
 
 const Main = () => {
   return (
-    <main className='main'>
-      <Promo />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-    </main>
+    <>
+      <Header authorized={ false } />
+      <main className='main'>
+        <Promo />
+        <AboutProject />
+        <Techs />
+        <AboutMe />
+      </main>
+      <Footer />
+    </>
   );
 }
 
