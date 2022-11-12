f8b9fe2d58551cda6e2ff43926e36538cf4f3625
HanmaDaiki <daikihanma@outlook.com>
2022-11-12 14:26:46 +0300

    fix: nvigate routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,66 +1,42 @@
-import React, { useState, Suspense } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import React, { Suspense, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Preloader from '../Preloader/Preloader';
+import PopupMenu from '../PopupMenu/PopupMenu';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import SettingsProfile from '../SettingsProfile/SettingsProfile';
 
-const Footer = React.lazy(() => import('../Footer/Footer'));
-const Header = React.lazy(() => import('../Header/Header'));
 const Main = React.lazy(() => import('../Main/Main'));
 const Movies = React.lazy(() => import('../Movies/Movies'));
 const Login = React.lazy(() => import('../Login/Login'));
 const Register = React.lazy(() => import('../Register/Register'));
 
 function App() {
-  const navigate = useNavigate();
-  const [authorized, setAuthorized] = useState(false);
+  const [openPopupMenu, setOpenPopupMenu] = useState(false);
 
-  function handleLogo() {
-    navigate('/');
+  function handleClosePopupMenu() {
+    setOpenPopupMenu(false);
   };
 
-  function handleRegistration() {
-    navigate('/signup');
-  };
-
-  function handleLogin() {
-    navigate('/signin');
-  };
-
-  function handleAccount() {
-    navigate('/profile')
-  };
-  
-  function handleFilms() {
-    navigate('/movies');
-  };
-
-  function handleSavedFilms() {
-    navigate('/saved-movies');
+  function handleOpenPopupMenu() {
+    setOpenPopupMenu(true);
   };
 
   return (
     <div className="app">
        <Suspense fallback={<Preloader />}>
-        <Header
-        handleLogoClick={ handleLogo } 
-        handleRegistrationClick={ handleRegistration }
-        handleLoginClick={ handleLogin }
-        handleAccountClick={ handleAccount }
-        handleFilmsClick={handleFilms}
-        handleSavedFilms={handleSavedFilms}
-        authorized={ authorized }
-        />
         <Routes>
             <Route path='/' element={ <Main /> }/>
             <Route path='/signin' element={ <Login /> }/>
             <Route path='/signup' element={ <Register /> }/>
-            <Route path='/movies' element={ <Movies /> } />
-            <Route path='/saved-movies' element={ <h1>Сохранённые фильмы </h1> } />
-            <Route path='/profile' element={ <h1>Профиль</h1> } />
+            <Route path='/movies' element={ <Movies handleOpenPopupMenu={ handleOpenPopupMenu }/> } />
+            <Route path='/saved-movies' element={ <SavedMovies handleOpenPopupMenu={ handleOpenPopupMenu } /> } />
+            <Route path='/profile' element={ <SettingsProfile handleOpenPopupMenu={ handleOpenPopupMenu } /> } />
         </Routes>
-        <Footer />
       </Suspense>
+
+      <PopupMenu state={ openPopupMenu } closePopup={ handleClosePopupMenu } />
     </div>
   );
 }
