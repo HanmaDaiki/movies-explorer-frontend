0574e3c4d9fdde890aa7bf2c8b1cf939ead58744
Alexander Merzlikin <daikihanma@outlook.com>
2022-11-19 18:35:34 +0300

    fix, add: login, register + backend connect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,38 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
-import Fieldset from '../Fieldset/Fieldset';
+import Logo from '../Logo/Logo';
 import './Login.css';
 
-const Login = () => {
+const Login = ({ handleLogUser }) => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  function handleChangeEmail(event) {
+    setEmail(event.target.value);
+  }
 
-  function handleClickReg() {
+  function handleChangePassword(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleLogUser(email, password);
+  }
+
+  function handleGoToReg() {
     navigate('/signup');
-  };
-
-  const inputs = [
-    {
-      name: 'E-mail',
-      type: 'email',
-      forName: 'email'
-    },
-    {
-      name: 'Пароль',
-      type: 'password',
-      forName: 'password'
-    }
-  ];
+  }
 
   return (
-    <form className='login'>
-      <Fieldset 
-        inputs={ inputs } 
-        text='Рады видеть!' 
-        buttonText='Войти' 
-        subText={`Ещё не зарегистрированы?`}
-        subButton={<Button buttonText='Регистрация' onClick={ handleClickReg } type='link-form' /> }
-        />
+    <form className='login' onSubmit={ handleSubmit }>
+      <fieldset className='login__fieldset'>
+        <Logo />
+        <h1 className='login__fieldset-title'>Рады видеть!</h1>
+        <div className='login__input'>
+          <label className='login__label' htmlFor='email'>E-mail</label>
+          <input 
+            className='login__input-field' 
+            onChange={ handleChangeEmail } 
+            value={ email } 
+            type='email' 
+            name='email' 
+            required 
+          />
+        </div>
+        <div className='login__input'>
+          <label className='login__label' htmlFor='password'>Пароль</label>
+          <input 
+            className='login__input-field' 
+            onChange={ handleChangePassword } 
+            value={ password } 
+            type='password' 
+            name='password' 
+            required 
+          />
+        </div>
+
+        <Button buttonText='Войти' buttonType='submit' type={`form`} />
+        <p className='login__subtext'>
+          Ещё не зарегистрированы? <Button buttonText='Регистрация' type='link-form' onClick={ handleGoToReg }/>
+        </p>
+      </fieldset>
     </form>
   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,43 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
-import Fieldset from '../Fieldset/Fieldset';
+import Logo from '../Logo/Logo';
 import './Register.css'
 
-const Register = () => {
+const Register = ({ handleRegUser }) => {
   const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  function handleChangeName(event) {
+    setName(event.target.value);
+  }
 
-  function handleClickEnter() {
+  function handleChangeEmail(event) {
+    setEmail(event.target.value);
+  }
+
+  function handleChangePassword(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleRegUser(email, password, name);
+  }
+
+  function handleGoToLog() {
     navigate('/signin');
   }
 
-  const inputs = [
-    {
-      name: 'Имя',
-      type: 'text',
-      forName: 'name'
-    },
-    {
-      name: 'E-mail',
-      type: 'email',
-      forName: 'email'
-    },
-    {
-      name: 'Пароль',
-      type: 'password',
-      forName: 'password'
-    }
-  ];
 
   return (
-    <form className='register'>
-      <Fieldset 
-        inputs={inputs}
-        text='Добро пожаловать!' 
-        buttonText='Зарегистрироваться' 
-        subText={`Уже зарегистрированы?`}
-        subButton={<Button buttonText='Войти' onClick={ handleClickEnter } type='link-form' /> }
-        />
+    <form className='register' onSubmit={ handleSubmit }>
+      <fieldset className='register__fieldset'>
+        <Logo />
+        <h1 className='register__fieldset-title'>Добро пожаловать!</h1>
+
+        <div className='register__input'>
+          <label className='register__label' htmlFor='name'>Имя</label>
+          <input 
+            className='register__input-field' 
+            onChange={ handleChangeName } 
+            value={ name }
+            type='text' 
+            name='name' 
+            required 
+          />
+        </div>
+        <div className='register__input'>
+          <label className='register__label' htmlFor='email'>E-mail</label>
+          <input 
+            className='register__input-field' 
+            onChange={ handleChangeEmail } 
+            value={ email } 
+            type='email' 
+            name='email' 
+            required 
+          />
+        </div>
+        <div className='register__input'>
+          <label className='register__label' htmlFor='password'>Пароль</label>
+          <input 
+            className='register__input-field' 
+            onChange={ handleChangePassword } 
+            value={ password } 
+            type='password' 
+            name='password' 
+            required 
+          />
+        </div>
+
+        <Button buttonText='Зарегистрироваться' buttonType='submit' type={`form`} />
+        <p className='register__subtext'>
+          Уже зарегистрированы? <Button buttonText='Войти' onClick={ handleGoToLog } type='link-form' />
+        </p>
+      </fieldset>
     </form>
   );
 }
